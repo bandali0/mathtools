bc4de95730b11e0a86fea277f6fc9a2a76de452a
Amin Bandali <me@aminbandali.com>
2014-05-17 18:08:45 -0400

    Move MainActivity to activity folder

--- app/src/main/java/com/aminbandali/mathtools/app/activity/MainActivity.java
+++ app/src/main/java/com/aminbandali/mathtools/app/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.aminbandali.mathtools.app;
+package com.aminbandali.mathtools.app.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -6,6 +6,7 @@ import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.aminbandali.mathtools.app.R;
 import com.aminbandali.mathtools.app.activity.FeedbackActivity;
 
 

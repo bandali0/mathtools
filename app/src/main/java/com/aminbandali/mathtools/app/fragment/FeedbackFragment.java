b948d954219be9ade7b84aca4b37560e6f4454c8
Amin Bandali <me@aminbandali.com>
2014-05-28 00:06:32 -0400

    Use @OnItemClick in FeedbackFragment

--- app/src/main/java/com/aminbandali/mathtools/app/fragment/FeedbackFragment.java
+++ app/src/main/java/com/aminbandali/mathtools/app/fragment/FeedbackFragment.java
@@ -37,6 +37,7 @@ import java.util.Map;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnItemClick;
 
 
 public class FeedbackFragment extends BaseFragment
@@ -70,55 +71,52 @@ public class FeedbackFragment extends BaseFragment
                 android.R.layout.simple_list_item_2, from, to);
         listView.setAdapter(adapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                switch (position) {
-                    case 0:
-                        Uri uri = Uri.parse("market://details?id=" + getActivity().getApplicationContext().getPackageName());
-                        Intent playintent = new Intent(Intent.ACTION_VIEW, uri);
-                        startActivity(playintent);
-                        break;
-                    case 1:
-                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://google.com/+AminBandali")));
-                        break;
-                    case 2:
-                        Intent twttrintent = null;
-                        try {
-                            // get the Twitter app if possible
-                            getActivity().getPackageManager().getPackageInfo("com.twitter.android", 0);
-                            twttrintent = new Intent(Intent.ACTION_VIEW, Uri.parse("twitter://user?user_id=1254283670"));
-                            twttrintent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                        } catch (Exception e) {
-                            // no Twitter app, revert to browser
-                            twttrintent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/aminban"));
-                        }
-                        startActivity(twttrintent);
-                        break;
-                    case 3:
-                        Intent emailintent = new Intent(Intent.ACTION_SEND);
-                        emailintent.setType("message/rfc822");
-                        emailintent.putExtra(Intent.EXTRA_EMAIL, new String[]{"me@aminbandali.com"});
-                        emailintent.putExtra(Intent.EXTRA_SUBJECT, "Math Tools Feedback");
-                        emailintent.putExtra(Intent.EXTRA_TEXT, "Hi Amin, I use Math Tools and here's my feedback:\n\n");
-                        try {
-                            startActivity(Intent.createChooser(emailintent, "Shoot me an Email!"));
-                        } catch (android.content.ActivityNotFoundException ex) {
-                            Toast.makeText(getActivity(), "There are no email clients installed.", Toast.LENGTH_SHORT).show();
-                            Uri mailuri = Uri.parse("mailto://me@aminbandali.com");
-                            Intent emailbrowserintent = new Intent(Intent.ACTION_VIEW, mailuri);
-                            startActivity(emailbrowserintent);
-                            break;
-                        }
-                    default:
-                        break;
-                }
-
-            }
-        });
-
         return view;
+    }
+
+    @OnItemClick(R.id.lVfeedback)
+    void onItemClick(AdapterView<?> parent, View view,
+                         int position, long id) {
+        switch (position) {
+            case 0:
+                Uri uri = Uri.parse("market://details?id=" + getActivity().getApplicationContext().getPackageName());
+                Intent playintent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(playintent);
+                break;
+            case 1:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://google.com/+AminBandali")));
+                break;
+            case 2:
+                Intent twttrintent = null;
+                try {
+                    // get the Twitter app if possible
+                    getActivity().getPackageManager().getPackageInfo("com.twitter.android", 0);
+                    twttrintent = new Intent(Intent.ACTION_VIEW, Uri.parse("twitter://user?user_id=1254283670"));
+                    twttrintent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                } catch (Exception e) {
+                    // no Twitter app, revert to browser
+                    twttrintent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/aminban"));
+                }
+                startActivity(twttrintent);
+                break;
+            case 3:
+                Intent emailintent = new Intent(Intent.ACTION_SEND);
+                emailintent.setType("message/rfc822");
+                emailintent.putExtra(Intent.EXTRA_EMAIL, new String[]{"me@aminbandali.com"});
+                emailintent.putExtra(Intent.EXTRA_SUBJECT, "Math Tools Feedback");
+                emailintent.putExtra(Intent.EXTRA_TEXT, "Hi Amin, I use Math Tools and here's my feedback:\n\n");
+                try {
+                    startActivity(Intent.createChooser(emailintent, "Shoot me an Email!"));
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(getActivity(), "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+                    Uri mailuri = Uri.parse("mailto://me@aminbandali.com");
+                    Intent emailbrowserintent = new Intent(Intent.ACTION_VIEW, mailuri);
+                    startActivity(emailbrowserintent);
+                    break;
+                }
+            default:
+                break;
+        }
     }
 
     private ArrayList<Map<String, String>> buildData() {

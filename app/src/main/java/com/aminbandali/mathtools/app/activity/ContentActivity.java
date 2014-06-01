fec876600cf3a18968b451acd9ea837c8a28eec9
Amin Bandali <me@aminbandali.com>
2014-06-01 17:21:38 -0400

    Move the temporary PlaceholderFragment into its own file

--- app/src/main/java/com/aminbandali/mathtools/app/activity/ContentActivity.java
+++ app/src/main/java/com/aminbandali/mathtools/app/activity/ContentActivity.java
@@ -38,6 +38,8 @@ import android.widget.RadioButton;
 import android.widget.TextView;
 
 import com.aminbandali.mathtools.app.R;
+import com.aminbandali.mathtools.app.fragment.PlaceholderFragment;
+
 import java.util.Locale;
 
 import butterknife.ButterKnife;
@@ -127,88 +129,6 @@ public class ContentActivity extends BaseActivity implements ActionBar.TabListen
     public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
     }
 
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        private static final String STATE_EQ_FORM_TEXT = "eq_form_text";
-        private static final String STATE_EQ_FORM_TAG = "eq_form_tag";
-
-        @InjectView(R.id.rB2D)
-        RadioButton rB2D;
-
-        @InjectView(R.id.rB3D)
-        RadioButton rB3D;
-
-        @InjectView(R.id.sEqForm)
-        TextView eqForm;
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_vectors_lines, container, false);
-            ButterKnife.inject(this, rootView);
-            doInit();
-            return rootView;
-        }
-
-        private void doInit() {
-            rB2D.setText(Html.fromHtml(getResources().getString(R.string.r2)));
-            rB3D.setText(Html.fromHtml(getResources().getString(R.string.r3)));
-        }
-
-        @OnClick(R.id.sEqForm)
-        void chooseEqForm(final TextView tV) {
-
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-
-            builder.setItems(R.array.eq_forms_array, new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int which) {
-                    // The 'which' argument contains the index position
-                    // of the selected item
-                    tV.setText(getResources().getStringArray(R.array.eq_forms_array)[which]);
-                    tV.setTag(which);
-                }
-            });
-
-            AlertDialog dialog = builder.create();
-            dialog.show();
-        }
-
-        @Override
-        public void onSaveInstanceState(Bundle outState) {
-            super.onSaveInstanceState(outState);
-            outState.putString(STATE_EQ_FORM_TEXT, eqForm.getText().toString());
-            outState.putString(STATE_EQ_FORM_TAG, eqForm.getTag().toString());
-        }
-
-        @Override
-        public void onActivityCreated(Bundle savedInstanceState) {
-            super.onActivityCreated(savedInstanceState);
-            if (savedInstanceState != null) {
-                eqForm.setText(savedInstanceState.getString(STATE_EQ_FORM_TEXT));
-                eqForm.setTag(savedInstanceState.getString(STATE_EQ_FORM_TAG));
-            }
-        }
-    }
 
     public class SectionsPagerAdapter extends FragmentStatePagerAdapter {
 
---
+++ app/src/main/java/com/aminbandali/mathtools/app/fragment/PlaceholderFragment.java
@@ -0,0 +1,119 @@
+/*
+ * PlaceholderFragment.java
+ * Copyright (C) 2014 Amin Bandali <me@aminbandali.com>
+ *
+ * MathTools is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MathTools is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.aminbandali.mathtools.app.fragment;
+
+import android.app.AlertDialog;
+import android.app.Fragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import com.aminbandali.mathtools.app.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+
+public class PlaceholderFragment extends Fragment {
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    private static final String STATE_EQ_FORM_TEXT = "eq_form_text";
+    private static final String STATE_EQ_FORM_TAG = "eq_form_tag";
+
+    @InjectView(R.id.rB2D)
+    RadioButton rB2D;
+
+    @InjectView(R.id.rB3D)
+    RadioButton rB3D;
+
+    @InjectView(R.id.sEqForm)
+    TextView eqForm;
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static PlaceholderFragment newInstance(int sectionNumber) {
+        PlaceholderFragment fragment = new PlaceholderFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public PlaceholderFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_vectors_lines, container, false);
+        ButterKnife.inject(this, rootView);
+        doInit();
+        return rootView;
+    }
+
+    private void doInit() {
+        rB2D.setText(Html.fromHtml(getResources().getString(R.string.r2)));
+        rB3D.setText(Html.fromHtml(getResources().getString(R.string.r3)));
+    }
+
+    @OnClick(R.id.sEqForm)
+    void chooseEqForm(final TextView tV) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder.setItems(R.array.eq_forms_array, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                // The 'which' argument contains the index position
+                // of the selected item
+                tV.setText(getResources().getStringArray(R.array.eq_forms_array)[which]);
+                tV.setTag(which);
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(STATE_EQ_FORM_TEXT, eqForm.getText().toString());
+        outState.putString(STATE_EQ_FORM_TAG, eqForm.getTag().toString());
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (savedInstanceState != null) {
+            eqForm.setText(savedInstanceState.getString(STATE_EQ_FORM_TEXT));
+            eqForm.setTag(savedInstanceState.getString(STATE_EQ_FORM_TAG));
+        }
+    }
+}

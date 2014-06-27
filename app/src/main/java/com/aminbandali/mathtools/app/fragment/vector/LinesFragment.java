f769d81189db9df33c3c221323a8564bb6da6fc3
Amin Bandali <me@aminbandali.com>
2014-06-26 22:16:37 -0400

    R2 and R3 toggles now work

--- app/src/main/java/com/aminbandali/mathtools/app/fragment/vector/LinesFragment.java
+++ app/src/main/java/com/aminbandali/mathtools/app/fragment/vector/LinesFragment.java
@@ -26,6 +26,8 @@ import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
@@ -53,6 +55,22 @@ public class LinesFragment extends Fragment {
 
     @InjectView(R.id.sEqForm)
     TextView eqForm;
+
+    @InjectView(R.id.linesll1) LinearLayout row1;
+    @InjectView(R.id.linestextx1) EditText x1;
+    @InjectView(R.id.linestexty1) EditText y1;
+    @InjectView(R.id.linestextz1) EditText z1;
+    @InjectView(R.id.linestextx2) EditText x2;
+    @InjectView(R.id.linestexty2) EditText y2;
+    @InjectView(R.id.linestextz2) EditText z2;
+
+    @InjectView(R.id.linesll2) LinearLayout row2;
+    @InjectView(R.id.linestextx3) EditText x3;
+    @InjectView(R.id.linestexty3) EditText y3;
+    @InjectView(R.id.linestextz3) EditText z3;
+    @InjectView(R.id.linestextx4) EditText x4;
+    @InjectView(R.id.linestexty4) EditText y4;
+    @InjectView(R.id.linestextz4) EditText z4;
 
     public LinesFragment() {
     }
@@ -87,6 +105,25 @@ public class LinesFragment extends Fragment {
 
         AlertDialog dialog = builder.create();
         dialog.show();
+    }
+
+    @OnClick(R.id.rB2D)
+    void chooseR2Space(RadioButton rB) {
+        z1.setVisibility(View.GONE);
+        z2.setVisibility(View.GONE);
+        z3.setVisibility(View.GONE);
+        z4.setVisibility(View.GONE);
+        row1.setWeightSum(4);
+        row2.setWeightSum(4);
+    }
+    @OnClick(R.id.rB3D)
+    void chooseR3Space(RadioButton rB) {
+        z1.setVisibility(View.VISIBLE);
+        z2.setVisibility(View.VISIBLE);
+        z3.setVisibility(View.VISIBLE);
+        z4.setVisibility(View.VISIBLE);
+        row1.setWeightSum(6);
+        row2.setWeightSum(6);
     }
 
     @Override

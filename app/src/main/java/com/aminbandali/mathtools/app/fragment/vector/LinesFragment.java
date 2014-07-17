95f32da8fda6a1839abc30482631ccc0f8f8be4e
amin bandali <me@aminbandali.com>
2014-07-17 19:34:31 -0400

    use the new duplicate fragments (prjs & lines)

--- app/src/main/java/com/aminbandali/mathtools/app/fragment/vector/LinesFragment.java
+++ app/src/main/java/com/aminbandali/mathtools/app/fragment/vector/LinesFragment.java
@@ -91,7 +91,7 @@ public class LinesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_vectors_products, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_vectors_lines, container, false);
         ButterKnife.inject(this, rootView);
         doInit();
         return rootView;
--- app/src/main/java/com/aminbandali/mathtools/app/fragment/vector/ProjectionsFragment.java
+++ app/src/main/java/com/aminbandali/mathtools/app/fragment/vector/ProjectionsFragment.java
@@ -91,7 +91,7 @@ public class ProjectionsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_vectors_products, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_vectors_projections, container, false);
         ButterKnife.inject(this, rootView);
         doInit();
         return rootView;

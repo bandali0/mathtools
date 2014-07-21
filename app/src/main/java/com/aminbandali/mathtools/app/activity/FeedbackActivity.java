6615dcf56390651f2be0c64185885febfc9c65b5
Amin Bandali <me@aminbandali.com>
2014-07-20 22:05:11 -0400

    Correct "Up" behaviour for FeedbackActivity

--- app/src/main/java/com/aminbandali/mathtools/app/activity/FeedbackActivity.java
+++ app/src/main/java/com/aminbandali/mathtools/app/activity/FeedbackActivity.java
@@ -18,7 +18,11 @@
 
 package com.aminbandali.mathtools.app.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v4.app.TaskStackBuilder;
+import android.view.MenuItem;
 
 import com.aminbandali.mathtools.app.R;
 
@@ -30,5 +34,23 @@ public class FeedbackActivity extends BaseActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
